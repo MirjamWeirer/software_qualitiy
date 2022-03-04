d81e973b9ba0bb74c488926b3954b01601568ece
MirjamWeirer <mirjam.weirer@gmx.at>
2022-03-04 09:43:04 +0100

    added tests NoShiftUpInGear6 and NoShiftDownInGear1

--- src/com/lagerweij/TestGearBox.java
+++ src/com/lagerweij/TestGearBox.java
@@ -25,4 +25,18 @@ public class TestGearBox {
         gearBox.shiftGear(400);
         Assert.assertEquals(2,gearBox.gear);
     }
+    @Test
+    public void testNoShiftUpInGear6(){
+        GearBox gearBox = new GearBox();
+        gearBox.gear = 6;
+        gearBox.shiftGear(3000);
+        Assert.assertEquals(6,gearBox.gear);
+    }
+    @Test
+    public void testNoShiftDownInGear1(){
+        GearBox gearBox = new GearBox();
+        gearBox.gear = 1;
+        gearBox.shiftGear(400);
+        Assert.assertEquals(1,gearBox.gear);
+    }
 }
